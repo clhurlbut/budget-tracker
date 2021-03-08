a6e7a4747b883112f59fff93442459e6009bee87
CassBut <clhurlbut@gmail.com>
2021-03-07 20:40:37 -0800

    installed and activated service worker

--- public/js/idb.js
+++ public/js/idb.js
@@ -31,7 +31,7 @@ function uploadTransaction() {
 
     getAll.onsuccess = function () {
         if (getAll.result.length > 0) {
-            fetch('/api/transactions/bulk', {
+            fetch('/api/transaction/bulk', {
                 method: 'POST',
                 body: JSON.stringify(getAll.result),
                 headers: {
